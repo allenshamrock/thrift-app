1d51b5e54b232a0670282a6676ef936195543cc1
Allen <allenshamrock37@gmail.com>
2024-09-23 13:10:57 +0300

    {Feat}:Added the PATCH & DELETE endpoints

--- api/views.py
+++ api/views.py
@@ -1,14 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from authentication.models import Customer,Order
-from .serializers import CustomerSerializer,OrderSerializer
+from authentication.models import Customer, Order
+from .serializers import CustomerSerializer, OrderSerializer
+from rest_framework.exceptions import NotFound
 
-
+# Customer API
 @api_view(['GET'])
 def getCustomers(request):
     customers = Customer.objects.all()
-    serializer =CustomerSerializer(customers, many=True)
+    serializer = CustomerSerializer(customers, many=True)
     return Response(serializer.data)
+
 
 api_view(['POST'])
 def addCustomer(request):
@@ -18,6 +20,31 @@ def addCustomer(request):
     return Response(serializer.data)
 
 
+@api_view(['PATCH'])
+def update_customer(request, customer_id):
+    try:
+        customer = Customer.objects.get(pk=customer_id)
+    except Customer.DoesNotExist:
+        raise NotFound('Customer not found')
+
+    serializer = CustomerSerializer(customer, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+
+@api_view(['DELETE'])
+def delete_customer(request, customer_id):
+    try:
+        customer = Customer.objects.get(pk=customer_id)
+    except Customer.DoesNotExist:
+        raise NotFound('Customer not found')
+
+    customer.delete()
+    return Response({'message': 'Customer deleted successfully'})
+
+# Order API
 @api_view(['GET'])
 def getOrders(request):
     customers = Order.objects.all()
@@ -26,11 +53,33 @@ def getOrders(request):
 
 
 api_view(['POST'])
-
-
 def addOrder(request):
     serializer = OrderSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
 
+
+@api_view(['PATCH'])
+def update_order(request, order_id):
+    try:
+        order = Order.objects.get(pk=order_id)
+    except Order.DoesNotExist:
+        raise NotFound('Order not found')
+    
+    serializer = OrderSerializer(order, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+
+@api_view(['DELETE'])
+def delete_order(request, order_id):
+    try:
+        order = Order.objects.get(pk=order_id)
+    except Order.DoesNotExist:
+        raise NotFound('Order not found')
+
+    order.delete()
+    return Response({'message': 'Order deleted successfully'})
